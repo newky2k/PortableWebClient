1b57ff21f8449c44c0c01b87a0d8493bc24f6340
Dave Humphreys <newky2k@mac.com>
2020-01-29 12:52:54 +0000

    Added detection of failed server connections

---
+++ DSoft.Portable.WebClient.Core/Exceptions/NoServerResponseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSoft.Portable.WebClient.Core.Exceptions
+{
+    public class NoServerResponseException : Exception
+    {
+        public NoServerResponseException() : base("Unable to connect to the server")
+        {
+
+        }
+    }
+}
