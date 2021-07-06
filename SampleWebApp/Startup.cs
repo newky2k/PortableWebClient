67eaba5e1591629f2df86df49156771dcb6fe00c
Dave Humphreys <newky2k@users.noreply.github.com>
2021-07-06 17:03:56 +0100

    Updated sample

--- SampleWebApp/Startup.cs
+++ SampleWebApp/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DSoft.Portable.WebClient.Encryption;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,6 +26,8 @@ namespace SampleWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            EncryptionProviderFactory.InitVector = "xRFg8Ctp1sEqWfVp";
+
             services.AddControllers();
         }
 
