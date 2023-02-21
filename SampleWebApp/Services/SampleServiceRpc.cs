329c080051b1f703119ac4d6788a31843b655ea5
David Humphreys <davidh@westwoodrock.com>
2023-02-21 19:55:05 +0000

    Added sample GRPC service to sampe web app

---
+++ SampleWebApp/Services/SampleServiceRpc.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using SampleRpc;
+using System.Threading.Tasks;
+
+namespace SampleWebApp.Services
+{
+	public class SampleServiceRpc : SampleRpc.SampleContract.SampleContractBase
+	{
+
+		public SampleServiceRpc()
+		{
+
+		}
+
+		public override Task<SimpleResponse> Find(SimpleRequest request, ServerCallContext context)
+		{
+			return base.Find(request, context);
+		}
+	}
+}
--- SampleWebApp/Startup.cs
+++ SampleWebApp/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SampleWebApp.Services;
 
 namespace SampleWebApp
 {
@@ -27,7 +28,9 @@ namespace SampleWebApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-        }
+
+			services.AddGrpc();
+		}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -41,12 +44,16 @@ namespace SampleWebApp
 
             app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseGrpcWeb();
+
+			app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-            });
+
+                endpoints.MapGrpcService<SampleServiceRpc>().EnableGrpcWeb();
+			});
         }
     }
 }
