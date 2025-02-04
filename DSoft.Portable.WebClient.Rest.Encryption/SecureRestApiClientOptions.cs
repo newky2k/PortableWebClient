0341cb87bd8d45f0b8a70e958727f0709d61edc1
David Humphreys <newky2k@users.noreply.github.com>
2025-02-04 18:34:33 +0000

    Renamed securerestclientoptions to SecureRestApiClientOptions

--- DSoft.Portable.WebClient.Rest.Encryption/SecureRestApiClientOptions.cs
+++ DSoft.Portable.WebClient.Rest.Encryption/SecureRestApiClientOptions.cs
@@ -8,7 +8,7 @@ namespace DSoft.Portable.WebClient.Rest.Encryption
     /// <summary>
     /// Security Options for the rest client
     /// </summary>
-    public class SecureRestClientOptions : RestApiClientOptions
+    public class SecureRestApiClientOptions : RestApiClientOptions
     {
         /// <summary>
         /// Gets or sets the size of the key for the encryption
