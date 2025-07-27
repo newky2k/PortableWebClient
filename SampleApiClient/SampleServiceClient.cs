43766f35ecc864c9085f41f7da0a47c29a1753c9
David Humphreys <newky2k@users.noreply.github.com>
2025-07-27 14:20:07 +0100

    Updated to remove client version no from options
    Made ClientVersionNo abstract on GrpcServiceClientBase

--- SampleApiClient/SampleServiceClient.cs
+++ SampleApiClient/SampleServiceClient.cs
@@ -3,12 +3,38 @@ using DSoft.Portable.WebClient.Grpc;
 using Microsoft.Extensions.Options;
 using SampleRpc;
 using System;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace SampleApiClient
 {
 	public class SampleServiceClient : GrpcServiceClientBase
 	{
+        #region Fields
+        private Version _appVersion;
+        #endregion
+
+        /// <summary>
+        /// Gets the client version no, from the assemblt
+        /// </summary>
+        /// <value>
+        /// The client version no.
+        /// </value>
+        protected override string ClientVersionNo
+        {
+            get
+            {
+                if (_appVersion == null)
+                {
+                    var asm = Assembly.GetAssembly(this.GetType());
+
+                    _appVersion = asm.GetName().Version;
+                }
+
+
+                return _appVersion.ToString();
+            }
+        }
 
         public SampleServiceClient(IGrpcChannelManager channelManager, IOptions<GrpcClientOptions> options) : base(channelManager, options)
         {
